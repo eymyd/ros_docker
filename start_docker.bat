7836965fb7e68d7794e4f1742b6315d9556de1cf
Christian Eymüller <christian@eymueller.de>
2018-10-24 14:20:25 +0200

    added windows .bat s

--- start_docker.bat
+++ start_docker.bat
@@ -1,1 +1,1 @@
-docker run --rm -p 6080:80 -p 5900:5900  -v $pwd/catkin_ws:/root/catkin_ws --name isse-ros -it  isse-ros
+docker run --rm -p 6080:80 -p 5900:5900 -v $pwd/catkin_ws:/root/catkin_ws --name isse-ros -it  isse-ros
