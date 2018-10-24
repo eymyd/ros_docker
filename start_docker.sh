a4184efa3c451c9b663a51e5fdf2c42dc5b50827
Christian Eymüller <christian@eymueller.de>
2018-10-24 17:29:44 +0200

    changed resolution

--- start_docker.sh
+++ start_docker.sh
@@ -4,5 +4,6 @@ docker run --rm \
 	-p 6080:80 -p 5900:5900 \
 	-v $(pwd)/catkin_ws:/root/catkin_ws \
 	-v $(pwd)/.conf/.gazebo:/root/.gazebo \
+	-e VNC_RESOLUTION=1920x1080 \
 	--name isse-ros \
 	-it  isse-ros
